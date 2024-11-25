a31fd7598b1a66c3db41e739420b30126862de2d
Abdikhafar Issack <abdikhafarissack@gmail.com>
2024-11-25 18:25:30 +0300

    Replaced react-typical with react-simple-typewriter

--- src/components/animationtext/TypingText.jsx
+++ src/components/animationtext/TypingText.jsx
@@ -2,9 +2,18 @@ import { motion } from "framer-motion";
 import { textContainer, textVariant2 } from "../../motion";
 
 export const TypingText = ({ title, textStyles }) => (
-	<motion.p variants={textContainer} className={`${textStyles}`}>
+	<motion.p
+		variants={textContainer}
+		initial="hidden"
+		animate="show"
+		className={`${textStyles}`}
+	>
 		{Array.from(title).map((letter, index) => (
-			<motion.span variants={textVariant2} key={index}>
+			<motion.span
+				variants={textVariant2}
+				key={index}
+				style={{ display: "inline-block" }}
+			>
 				{letter === " " ? "\u00A0" : letter}
 			</motion.span>
 		))}
